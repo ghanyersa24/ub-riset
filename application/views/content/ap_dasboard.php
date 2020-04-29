2617eda0e190e2aad117e74be125f080605158fd
Ghany Abdillah Ersa <ghany.ersa@bigio.id>
2020-04-30 01:07:58 +0700

    add produk in dashboard

--- application/views/content/ap_dasboard.php
+++ application/views/content/ap_dasboard.php
@@ -11,7 +11,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 			<div class="col-12">
 				<div class="card">
 					<div class="card-body">
-						<p class="card-text">Sudah mempunyai <strong class="px-1">ide</strong> atau <strong class="px-1">produk</strong>  inovasimu? <a href="#" class="ml-2 text-decoration-none" id="add-product"> klik disini</a></p>
+						<p class="card-text">Sudah mempunyai <strong class="px-1">ide</strong> atau <strong class="px-1">produk</strong> inovasimu? <a href="#" class="ml-2 text-decoration-none" id="add-product"> klik disini</a></p>
 					</div>
 				</div>
 			</div>
@@ -77,20 +77,72 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	</section>
 </div>
 <div id="add" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-	<div class="modal-dialog modal-dialog-centered" role="document">
+	<div class="modal-dialog modal-dialog-centered modal-xl" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
 				<h5 class="modal-title" id="exampleModalLabel">Daftarkan !!!</h5> <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
 			</div>
 			<div class="modal-body">
-				<div class="form-group">
-					<label for="view-name">Nama Produk</label>
-					<input type="text" class="form-control" id="view-name" name="name" value="Produk Inovasi I">
-				</div>
-				<div class="form-group">
-					<label for="view-name">Deskripsi Produk</label>
-					<textarea name="spesifikasi" id="spesifikasi" class="form-control"></textarea>
-				</div>
+				<form id="form-add">
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="form-group">
+								<label for="view-nama_produk">Nama Produk</label>
+								<input type="text" class="form-control" id="view-nama_produk" name="nama_produk" placeholder="Brain Apps">
+							</div>
+							<div class="form-group">
+								<label for="view-jenis">Jenis Produk</label>
+								<div class="row container">
+									<div class="col custom-control custom-radio">
+										<input type="radio" id="view-jenis1" name="jenis" class="custom-control-input" value="digital" checked>
+										<label class="custom-control-label" for="view-jenis1">Digital</label>
+									</div>
+									<div class="col custom-control custom-radio">
+										<input type="radio" id="view-jenis2" name="jenis" class="custom-control-input" value="non digital">
+										<label class="custom-control-label" for="view-jenis2">Non Digital</label>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-8">
+							<div class="form-group">
+								<label for="add-deskripsi">Deskripsi Singkat Produk</label>
+								<textarea name="deskripsi_singkat" id="add-deskripsi_singkat" class="form-control" placeholder="Sistem manajemen brawijaya dalam bidang riset dan inovasi"></textarea>
+							</div>
+							<div class="form-group">
+								<label for="add-bidang">Bidang</label>
+								<select class="custom-select" name="bidang">
+									<option selected disabled>Pilih salah satu</option>
+									<option value="Pangan">Pangan</option>
+									<option value="Energi">Energi</option>
+									<option value="Transportasi">Transportasi</option>
+									<option value="Rekayasa Keteknikan">Rekayasa Keteknikan</option>
+									<option value="Kesehatan">Kesehatan</option>
+									<option value="Pertahanan Keamanan">Pertahanan Keamanan</option>
+									<option value="Material Maju">Material Maju</option>
+									<option value="Kemaritiman">Kemaritiman</option>
+									<option value="Sosial Budaya">Sosial Budaya</option>
+								</select>
+							</div>
+
+							<div class="form-group">
+								<label for="add-kategori">Kategori</label>
+								<select class="custom-select" name="kategori">
+									<option selected disabled>Pilih salah satu</option>
+									<option value="Pangan">Pangan</option>
+									<option value="Energi">Energi</option>
+									<option value="Transportasi">Transportasi</option>
+									<option value="Rekayasa Keteknikan">Rekayasa Keteknikan</option>
+									<option value="Kesehatan">Kesehatan</option>
+									<option value="Pertahanan Keamanan">Pertahanan Keamanan</option>
+									<option value="Material Maju">Material Maju</option>
+									<option value="Kemaritiman">Kemaritiman</option>
+									<option value="Sosial Budaya">Sosial Budaya</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</form>
 			</div>
 			<div class="modal-footer">
 				<button class="btn btn-default" data-dismiss="modal">Batal</button>
@@ -105,10 +157,21 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	});
 
 	$('#btn-save').click(function(e) {
-		swal('Success !', 'data produk berhasil ditambahkan', 'success');
-		$('#add').modal('hide');
-		setTimeout(function() {
-			window.location.replace('http://localhost/ub-riset/admin/detail/I')
-		}, 2000)
+		$.ajax({
+			type: "POST",
+			url: api + 'service/produk/create',
+			data: $('#form-add').serialize(),
+			success: function(response) {
+				if (!response.error) {
+					swal('Success !', response.message, 'success')
+					$('#add').modal('hide')
+					setTimeout(function() {
+						window.location.replace('http://localhost/ub-riset/admin/detail/I')
+					}, 2000)
+				} else
+					swal('Gagal !', response.message, 'error')
+			}
+		});
+
 	});
 </script>
