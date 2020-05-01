00f564319b4868235f78093d38cee016947136cc
Fawwaz Daffa Muhammad <fawwazdaffam99@gmail.com>
2020-05-01 16:18:28 +0700

    add research page

---
+++ application/views/content/ap_research.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+
+<div class="main-content">
+    <section class="section">
+        <div class="section-header">
+            <h1><?= $title ?></h1>
+        </div>
+
+        <div class="section-body">
+            <div class="card">
+                <p>Hello world</p>
+            </div>
+        </div>
+    </section>
+</div>
