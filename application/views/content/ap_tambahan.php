36cd3a38826aba8dbd2b4601b2eb2c35f65b3342
Fawwaz Daffa Muhammad <fawwazdaffam99@gmail.com>
2020-05-11 01:09:12 +0700

    add static detail

---
+++ application/views/content/ap_tambahan.php
@@ -0,0 +1,24 @@
+<div class="main-content">
+    <section class="section">
+        <div class="section-header d-block justify-content-start align-items-center">
+
+            <h1 class="pt-2 pb-2 mt-0 ml-3"><?= $title ?></h1>
+        </div>
+        <button class="btn btn-info " data-toggle="modal" data-target="#add" style="position: fixed; bottom: 36px;   right: 20px; padding: 18.5px;z-index: 10;">
+            <i class="fa fa-plus"></i>
+        </button>
+
+
+        <div class="section-body">
+            <div class="row mt-sm-4">
+                <div class="col-12 col-md-12 col-lg-12">
+                    <div class="card">
+                        <div class="card-body row" id="tambahan">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
