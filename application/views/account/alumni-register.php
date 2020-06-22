5caf351261c4afdad9858032d9fe75df43386e52
fawwaz daffa <fawwazdaffam99@gmail.com>
2020-06-22 12:39:19 +0700

    fixing view alumni-register

--- application/views/account/alumni-register.php
+++ application/views/account/alumni-register.php
@@ -35,56 +35,57 @@
 	</style>
 </head>
 
-<body class="overflow-hidden">
-	<div class="row vh-100">
+<body class="overflow-scroll">
+	<div class="row min-vh-100">
 		<div class="col-md-6 text-center d-flex align-items-center">
 			<div class="col text-center d-flex justify-content-center">
 				<img src="<?= base_url() . 'assets/img/logo.png' ?>" alt="" class="w-50 rounded d-none d-md-block">
 			</div>
 		</div>
 		<div class="col-md-6 bg-primary d-flex align-items-center">
-			<div class="col-md-8 offset-2">
-				<p class="h4 text-white">Register Account</p>
-				<form class="form-add" id="form-add">
-					<div class="form-group">
-						<label for="add-nama_lengkap">Nama Lengkap</label>
-						<input id="add-nama_lengkap" class="form-control" type="text" name="nama_lengkap" value="" required>
-					</div>
-					<div class="form-group">
-						<label for="add-email">Email</label>
-						<input id="add-email" class="form-control" type="text" name="email" value="" required>
-					</div>
-					<div class="row">
-						<div class="col-md-6">
-							<div class="form-group">
-								<label for="add-password">Password</label>
-								<input id="add-password" class="form-control" type="password" name="password" value="" required>
+			<div class="col-md-8 offset-md-2">
+				<div class="container">
+					<p class="h4 text-white">Register Account</p>
+					<form class="form-add" id="form-add">
+						<div class="form-group">
+							<label for="add-nama_lengkap">Nama Lengkap</label>
+							<input id="add-nama_lengkap" class="form-control" type="text" name="nama_lengkap" value="" required>
+						</div>
+						<div class="form-group">
+							<label for="add-email">Email</label>
+							<input id="add-email" class="form-control" type="text" name="email" value="" required>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="add-password">Password</label>
+									<input id="add-password" class="form-control" type="password" name="password" value="" required>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="add-password_konfirmasi">Konfirmasi Password</label>
+									<input id="add-password_konfirmasi" class="form-control" type="password" name="password_konfirmasi" value="" required>
+								</div>
 							</div>
 						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<label for="add-password_konfirmasi">Konfirmasi Password</label>
-								<input id="add-password_konfirmasi" class="form-control" type="password" name="password_konfirmasi" value="" required>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="add-kontak">No. HP</label>
+									<input id="add-kontak" class="form-control" type="number" name="kontak" required>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="add-nim">NIM saat menjadi mahasiswa</label>
+									<input id="add-nim" class="form-control" type="number" name="nim" required>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-6">
-							<div class="form-group">
-								<label for="add-kontak">No. HP</label>
-								<input id="add-kontak" class="form-control" type="number" name="kontak" required>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<label for="add-nim">NIM saat menjadi mahasiswa</label>
-								<input id="add-nim" class="form-control" type="number" name="nim" required>
-							</div>
-						</div>
-					</div>
-					<div class="row">
+						<div class="row">
 
-						<!-- <div class="col-md-4">
+							<!-- <div class="col-md-4">
                             <div class="form-group">
                                 <label for="add-identifier">Status</label>
                                 <select id="add-status" class="form-control" name="status">
@@ -94,56 +95,58 @@
                                 </select>
                             </div>
                         </div> -->
-					</div>
-					<div class="row">
-						<div class="col-4">
-							<div class="form-group">
-								<label for="add-fakultas">Fakultas</label>
-								<select class="custom-select" required name="fakultas">
-									<option value="FH">Hukum </option>
-									<option value="FEB">Ekonomi dan Bisnis</option>
-									<option value="FIA">Ilmu Administrasi</option>
-									<option value="FP">Pertanian </option>
-									<option value="FAPET">Peternakan </option>
-									<option value="FT">Teknik </option>
-									<option value="FK">Kedokteran </option>
-									<option value="FPIK">Perikanan dan Ilmu Kelautan </option>
-									<option value="FMIPA">Matematika dan IPA</option>
-									<option value="FTP">Teknologi Pertanian </option>
-									<option value="FISIP">Ilmu Sosial dan Ilmu Politik</option>
-									<option value="FIB">Ilmu Budaya</option>
-									<option value="FKH">Kedokteran Hewan</option>
-									<option value="FILKOM">Ilmu Komputer</option>
-									<option value="FKG">Kedokteran Gigi</option>
-									<option value="Vokasi">Program Vokasi</option>
-								</select> </div>
 						</div>
-						<div class="col-4">
-							<div class="form-group">
-								<label for="add-jurusan">Jurusan</label>
-								<input id="add-jurusan" class="form-control" type="text" name="jurusan" required>
+						<div class="row">
+							<div class="col-4">
+								<div class="form-group">
+									<label for="add-fakultas">Fakultas</label>
+									<select class="custom-select" required name="fakultas">
+										<option value="FH">Hukum </option>
+										<option value="FEB">Ekonomi dan Bisnis</option>
+										<option value="FIA">Ilmu Administrasi</option>
+										<option value="FP">Pertanian </option>
+										<option value="FAPET">Peternakan </option>
+										<option value="FT">Teknik </option>
+										<option value="FK">Kedokteran </option>
+										<option value="FPIK">Perikanan dan Ilmu Kelautan </option>
+										<option value="FMIPA">Matematika dan IPA</option>
+										<option value="FTP">Teknologi Pertanian </option>
+										<option value="FISIP">Ilmu Sosial dan Ilmu Politik</option>
+										<option value="FIB">Ilmu Budaya</option>
+										<option value="FKH">Kedokteran Hewan</option>
+										<option value="FILKOM">Ilmu Komputer</option>
+										<option value="FKG">Kedokteran Gigi</option>
+										<option value="Vokasi">Program Vokasi</option>
+									</select> </div>
+							</div>
+							<div class="col-4">
+								<div class="form-group">
+									<label for="add-jurusan">Jurusan</label>
+									<input id="add-jurusan" class="form-control" type="text" name="jurusan" required>
+								</div>
+							</div>
+							<div class="col-4">
+								<div class="form-group">
+									<label for="add-prodi">Prodi</label>
+									<input id="add-prodi" class="form-control" type="text" name="prodi" required>
+								</div>
 							</div>
 						</div>
-						<div class="col-4">
-							<div class="form-group">
-								<label for="add-prodi">Prodi</label>
-								<input id="add-prodi" class="form-control" type="text" name="prodi" required>
-							</div>
+						<div class="form-group">
+							<label for="add-bukti">Foto bukti kelulusan (Surat yudisium/KTA/Surat Keterangan Lulus)</label>
+							<input type="file" id="add-bukti" name="bukti" class="form-control">
 						</div>
+						<div class="d-flex justify-content-end">
+							<button type="submit" class="btn btn-light text-right">Submit</button>
+						</div>
+					</form>
+					<div class="text-center text-white mt-3 ml-3">
+						<span> Copyright &copy; Brawijaya</span> <span> Research and Innovation</span>
 					</div>
-					<div class="form-group">
-						<label for="add-bukti">Foto bukti kelulusan (Surat yudisium/KTA/Surat Keterangan Lulus)</label>
-						<input type="file" id="add-bukti" name="bukti" class="form-control">
-					</div>
-					<div class="d-flex justify-content-end">
-						<button type="submit" class="btn btn-light text-right">Submit</button>
-					</div>
-				</form>
+				</div>
 			</div>
+
 		</div>
-	</div>
-	<div class="text-center fixed-bottom mb-3 ml-3">
-		<span class="text-default"> Copyright &copy; Brawijaya</span> <span class="text-white"> Research and Innovation</span>
 	</div>
 	<script>
 		$('#form-add').validate({
